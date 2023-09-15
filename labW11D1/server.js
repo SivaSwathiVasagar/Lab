e35d465988802e55909bd1ed4b6140fa4853eea5
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-09-15 15:07:40 -0400

    un-commenting out

--- labW11D1/server.js
+++ labW11D1/server.js
@@ -3,20 +3,20 @@ const app = express();
 
 //////////////////      Greeting express application created    ///////////////////
 
-// app.get("/greeting/", function (req, res) {
-//   res.send("<h1>Hello, stranger</h1>");
-// });
+app.get("/greeting/", function (req, res) {
+  res.send("<h1>Hello, stranger</h1>");
+});
 
-// app.get("/greeting/:name", function (req, res) {
-// //   console.log(req.params); // check the params value.
-//   res.send("Wow! Hello there " + req.params.name);
-// });
+app.get("/greeting/:name", function (req, res) {
+//   console.log(req.params); // check the params value.
+  res.send("Wow! Hello there " + req.params.name);
+});
 
 ////////////////  Tip Calculator express application created   ////////////////
 
-// app.get("/tip/:total/:tipPercentage", (req, res) => {
-//   res.send("How much your tip will be is " + req.params.tipPercentage);
-// });
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  res.send("How much your tip will be is " + req.params.tipPercentage);
+});
 
 /////////////////////// Magic 8 Ball express application created ////////////////////
 
