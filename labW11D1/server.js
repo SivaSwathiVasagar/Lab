3b63e4254a3491d119b5d11a141fecf85674b116
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-09-15 14:51:15 -0400

    Tip Calculator express application created

--- labW11D1/server.js
+++ labW11D1/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/greeting/", function (req, res) {
-  res.send("<h1>Hello, stranger</h1>");
-});
+//////////////////      Greeting express application created    ///////////////////
+// app.get("/greeting/", function (req, res) {
+//   res.send("<h1>Hello, stranger</h1>");
+// });
 
-app.get("/greeting/:name", function (req, res) {
-//   console.log(req.params); // check the params value.
-  res.send("Wow! Hello there " + req.params.name);
+// app.get("/greeting/:name", function (req, res) {
+// //   console.log(req.params); // check the params value.
+//   res.send("Wow! Hello there " + req.params.name);
+// });
+
+////////////////  Tip Calculator express application created   ////////////////
+
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  res.send("How much your tip will be is " + req.params.tipPercentage);
 });
 
 // port setup
