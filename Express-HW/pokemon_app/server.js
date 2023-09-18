8bc8059d572ae06178ea6676f6fe7e52e8715028
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-09-18 14:24:45 -0400

    Connected my database, can see json in the browser

--- Express-HW/pokemon_app/server.js
+++ Express-HW/pokemon_app/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const pokemon = require("./models/pokemon.js");
+const jsxEngine = require("jsx-view-engine");
+
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine());
 
 app.get("/", (req, res) => {
   res.send(`<h1>Welcome to the Pokemon App!</h1>`);
+});
+
+app.get("/pokemon/", (req, res) => {
+  res.send(pokemon);
 });
 
 app.listen(port, () => {
